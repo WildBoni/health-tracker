f5e8f7eae4d2fc45991cedbff0a90bf5e65a71ec
Davide Bonifacio - Studiokey <web@davidebonifacio.it>
2016-11-15 18:13:49 +0100

    Structure: creating model

---
+++ README.md
@@ -0,0 +1,13 @@
+# Health Tracker with Backbone
+
+This web application tracks the user's calorie intake: it retrieves nutritional infos through Nutritionix APIs.
+I built this app as a project for Udacity Front End Web Developer Nanodegree.
+## Technologies used
+
+* JavaScript
+* Backbone
+* Underscore
+* Jquery
+* Bower
+
+# Health Tracker is under development
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-// useful Backbone tutorial: http://adrianmejia.com/blog/2012/09/11/backbone-dot-js-for-absolute-beginners-getting-started/
+// useful Backbone tutorials: 
+// http://adrianmejia.com/blog/2012/09/11/backbone-dot-js-for-absolute-beginners-getting-started/
+// http://tutorialzine.com/2013/04/services-chooser-backbone-js/
+$(function() {
+  var Meal = Backbone.Model.extend({
+    defaults:{
+      title: 'meal type',
+      calories: 'meal calories',
+      selected: false
+    },
+
+    toggle: function() {
+      this.set('selected', !this.get('selected'));
+    }
+  });
+});
