3ae9a1aa76cf6693cee142a97333037836eb87ef
Davide Bonifacio <web@davidebonifacio.it>
2016-11-19 18:24:44 +0100

    Feature: search field creating list and refreshing view

--- src/js/script.js
+++ src/js/script.js
@@ -65,6 +65,11 @@ $(function() {
     // assign every result to its MealView
     rendermeal: function(meal) {
       var mealview;
+      // clearing the list before updating it with new data
+      // thanks to andrewR and its suggestion in this thread
+      // https://discussions.udacity.com/t/cannot-delete-old-results-when-a-new-search-item-is-submitted/199056/4
+      const _meal_list = this.$el.find('#meal-list');
+      _meal_list.html('');
       for (var n in meal.models) {
         mealview = new MealView({model: meal.models[n]});
         // MealView is a <li> that goes inside its <ul>
